82d3502f224662c326aed9bca168714e6a6a1699
Victor Matheus <victormatheus507@gmail.com>
2025-02-21 13:03:22 -0300

    feat: add home page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -14,7 +14,7 @@ export const InputRoot = ({ error = false, ...rest }: InputRootProps) => {
   )
 }
 
-export const Inputicon = (props: ComponentProps<"span">) => {
+export const InputIcon = (props: ComponentProps<"span">) => {
   return (
     <span
       className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
