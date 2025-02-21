1a8b9a9ec2c10aa3d7614f31ad012bdb8f6bd4d4
Victor Matheus <victormatheus507@gmail.com>
2025-02-21 12:13:27 -0300

    feat: add inputs components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/Button';
 import { IconButton } from '@/components/IconButton';
-import { ArrowRight, Copy } from 'lucide-react';
+import { InputFild, Inputicon, InputRoot } from '@/components/Input';
+import { ArrowRight, Copy, Mail } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -14,6 +15,16 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <div>
+        <InputRoot>
+          <Inputicon>
+            <Mail className="size-5" />
+          </Inputicon>
+
+          <InputFild />
+        </InputRoot>
+      </div>
     </main>
   );
 }
