cd611f545d01c73d90a7733c855fcfed445c513b
Victor Matheus <victormatheus507@gmail.com>
2025-02-21 21:46:15 -0300

    feat: Invite Page

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,9 +1,16 @@
 import type { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
 
-export const IconButton = (props: ComponentProps<"button">) => {
+export const IconButton = ({
+  className,
+  ...props
+}: ComponentProps<"button">) => {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer  transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className={twMerge(
+        "p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer  transition-colors duration-300 hover:bg-blue hover:text-gray-900",
+        className
+      )}
       {...props}
     />
   )
